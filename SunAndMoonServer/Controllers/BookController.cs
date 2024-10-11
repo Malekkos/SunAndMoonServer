1c702bba07773ba5ba4af6f1d33e56d08c706577
Christian Anderson <tehninjawaffle@gmail.com>
2024-10-11 16:05:26 -0700

    Setup the controller for the first request, a get. Currently, responds with static information

---
+++ SunAndMoonServer/Controllers/BookController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using SunAndMoonServer.Models;
+
+namespace SunAndMoonServer.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+        public class BookAPIController : ControllerBase
+        {
+            [HttpGet]
+            public async Task<IActionResult> GetAllBooks()
+            {
+                var books = new List<Book>
+                {
+                    new() {
+                        Id = 1,
+                        Title = "Heralds of Andross",
+                        ChapterCount = 651,
+                        Completed = true,
+                        Author = "Mark Ruffalo",
+                        Description = "A whole lotta flavor text!"
+                    }
+                };
+
+                return Ok(books);
+            }
+        }
+        //public IActionResult Index()
+        //{
+        //    return View();
+        //}
+    }
